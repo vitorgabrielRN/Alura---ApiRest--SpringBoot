ad8fdec29efe8863a77ff4753d2ccdab5c9e2fe3
Vitor gabriel <vitorgabrieltrabalho@gmail.com>
2025-05-19 13:34:48 -0300

    ajustes SQL

--- src/main/java/crud/novamente/crud/CrudApplication.java
+++ src/main/java/crud/novamente/crud/CrudApplication.java
@@ -9,7 +9,12 @@ public class CrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 		System.out.println("funciona");
+
 	}
-	
+	//TODO ajustar os codigos para o banco de dados mysql Workbench
+	//TODO senha para isso é mysql -u root p senha root
+	// create database o nome que esta o appro.
+	// dps só rodar tudo pra ver se funciona
+	// pensar em atualizar isso pro desafio usar o mysql no lugar do postgres
 
 }
